e32e340268fd707633dc30a63680bf48417927b8
Yuishun <shun0ego@gmail.com>
2019-10-25 16:20:47 +0900

    c

---
+++ Assets/Scenes/GameScene/Scripts/Light.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    private GameObject[] CenterObjects;
+    private GameObject[] ShadowObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetCenterObj();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GetCenterObj()
+    {
+        CenterObjects = GameObject.FindGameObjectsWithTag("CenterObject");
+
+        ShadowObjects = GameObject.FindGameObjectsWithTag("ShadowObject");
+        foreach(GameObject game in ShadowObjects)
+        {
+            game.SetActive(false);
+        }
+    }
+
+    void CreateShadow()
+    {
+
+    }
+
+    void MoveShadow()
+    {
+
+    }
+}
