effcc42cc03a1bf7682f04068d4bcd8d1303fa79
yoshiki-shimada <yothiki321@gmail.com>
2019-11-01 19:03:35 +0900

    20191101昨日＿ハロウィンだったわ

--- Assets/Scenes/GameScene/ObjectData/SpawnManager.cs
+++ Assets/Scenes/GameScene/ObjectData/SpawnManager.cs
@@ -8,7 +8,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject entityToSpawn;
 
     //! 上のゲームScriptableObjectのインスタンス
-    public ObjectsData spawnManaferValues;
+    public ObjectsData spawnManagerValues;
 
     //! インスタンスナンバー
     int instanceNumber = 1;
@@ -23,6 +23,19 @@ public class SpawnManager : MonoBehaviour
     {
         int currentSpawnPointIndex = 0;
 
+        for(int i = 0;i < spawnManagerValues.numberOfPrefabsToCreate; i++)
+        {
+            // Creates an instance of the prefab at the current spawn point.
+            GameObject currentEntity = Instantiate(entityToSpawn, spawnManagerValues.spawnPoints[currentSpawnPointIndex], Quaternion.identity);
+
+            // Sets the name of the instantiated entity to be the string defined in the ScriptableObject and then appends it with a unique number. 
+            currentEntity.name = spawnManagerValues.prefabName + instanceNumber;
+
+            // Moves to the next spawn point index. If it goes out of range, it wraps back to the start.
+            currentSpawnPointIndex = (currentSpawnPointIndex + 1) % spawnManagerValues.spawnPoints.Length;
+
+            instanceNumber++;
+        }
 
     }
 }
---
+++ Assets/Scenes/GameScene/ObjectData/Tree1.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree1 : MonoBehaviour
+{
+    [SerializeField]
+    ObjectsData Tree1Data;
+}
+
