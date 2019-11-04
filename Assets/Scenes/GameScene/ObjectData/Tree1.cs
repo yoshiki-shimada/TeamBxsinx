1b63caae54c3912db53664162e6f0390b28f4a2e
yoshiki-shimada <yothiki321@gmail.com>
2019-11-04 11:10:33 +0900

    o

--- Assets/Scenes/GameScene/ObjectData/Tree1.cs
+++ Assets/Scenes/GameScene/ObjectData/Tree1.cs
@@ -18,9 +18,9 @@ public class Tree1 : MonoBehaviour
         GameObject t = Instantiate(Tree1Prefab, Tree1Data.spawnPoints[0],Tree1Data.q) as GameObject;
         
         //! animation
-        iTween.MoveAdd(t, iTween.Hash("y",Tree1Data.OInitAnim[0].y,"time",2f));
-        iTween.MoveAdd(t, iTween.Hash("x", Tree1Data.OInitAnim[1].x, "time", 1f, "delay", 2f));
-        iTween.MoveAdd(t, iTween.Hash("y", Tree1Data.OInitAnim[2].y, "time", 1f, "delay", 4.5f));
+        iTween.MoveBy(t, iTween.Hash("y",Tree1Data.OInitAnim[0].y,"time",2f));
+        iTween.MoveAdd(t, iTween.Hash("x", Tree1Data.OInitAnim[1].x, "time", 1f, "delay", 1f));
+        iTween.MoveAdd(t, iTween.Hash("y", Tree1Data.OInitAnim[2].y, "time", 1f, "delay", 2f));
     }
 }
 
