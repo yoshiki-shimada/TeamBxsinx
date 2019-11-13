4b229e2eaf745f4340f6b778b01901ff3842e183
yoshiki-shimada <yothiki321@gmail.com>
2019-11-13 19:14:28 +0900

    20191113_うんち

---
+++ Assets/Scenes/GameScene/ObjectData/Maruo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maruo : MonoBehaviour
+{
+    public GameObject MaruoPrefab;
+
+    public ObjectsData ShadowObjectData;
+
+    public void Spawn()
+    {
+        //! CloneInstance
+        GameObject t = Instantiate(MaruoPrefab, ShadowObjectData.spawnPoints[0], ShadowObjectData.q) as GameObject;
+
+        //! animation
+        iTween.MoveBy(t, iTween.Hash("x", ShadowObjectData.OInitAnim[0].x, "time", 1f));
+    }
+}
