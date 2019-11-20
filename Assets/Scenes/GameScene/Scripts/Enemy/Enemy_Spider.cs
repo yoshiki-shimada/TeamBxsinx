286092bc4a3d5784804e8234f254456493880be4
Yuishun <shun0ego@gmail.com>
2019-11-20 20:01:39 +0900

    2019/11/20 ライト交換の動き変更

---
+++ Assets/Scenes/GameScene/Scripts/Enemy/Enemy_Spider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Spider : MonoBehaviour
+{
+    SphereCollider Collider;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider = GetComponent<SphereCollider>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "ShadowObject")
+        {
+            Collider.isTrigger = false;
+            rb.isKinematic = false;
+        }
+    }
+}
