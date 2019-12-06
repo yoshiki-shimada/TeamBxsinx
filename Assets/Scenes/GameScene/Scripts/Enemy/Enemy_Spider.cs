99ed0655e000a8966050b6f624b2831ff51af4f8
Yuishun <shun0ego@gmail.com>
2019-12-06 15:53:13 +0900

    Playerに ReSet 追加

--- Assets/Scenes/GameScene/Scripts/Enemy/Enemy_Spider.cs
+++ Assets/Scenes/GameScene/Scripts/Enemy/Enemy_Spider.cs
@@ -4,19 +4,48 @@ using UnityEngine;
 
 public class Enemy_Spider : MonoBehaviour
 {
-    SphereCollider Collider;
+    CapsuleCollider Collider;
     Rigidbody rb;
+    Transform Balance;
     // Start is called before the first frame update
     void Start()
     {
-        Collider = GetComponent<SphereCollider>();
+        Collider = GetComponent<CapsuleCollider>();
         rb = GetComponent<Rigidbody>();
+        Balance = transform.GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position =
+                        transform.position +
+                        (-transform.right * 3 * Time.fixedDeltaTime);
+        RaycastHit hit;
+
+        // Transformの真下の地形の法線を調べる
+        if (Physics.Raycast(
+                    Balance.position,
+                    -transform.up,
+                    out hit,
+                    float.PositiveInfinity))
+        {
+            // 傾きの差を求める
+            Quaternion q = Quaternion.FromToRotation(
+                        transform.up,
+                        hit.normal);
+
+            // 自分を回転させる
+            transform.rotation *= q;
+
+            // 地面から一定距離離れていたら落下
+            if (hit.distance > 0.05f)
+            {
+                transform.position =
+                    transform.position +
+                    (-transform.up * Physics.gravity.magnitude * Time.fixedDeltaTime);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
