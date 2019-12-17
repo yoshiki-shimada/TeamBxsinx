52e1be115f15fdbd51267e657331c8055ac470b8
kajiringo-blu <kzyk1117@gmail.com>
2019-12-17 19:57:52 +0900

    2019/12/17...タイトル以外のサウンドオブジェ削除、ループしたとき音が重なってないのを確認

--- Assets/Scenes/GameOver/Scripts/LogoAnim.cs
+++ Assets/Scenes/GameOver/Scripts/LogoAnim.cs
@@ -18,7 +18,7 @@ public class LogoAnim : MonoBehaviour
         CreentCatIn = (GameObject)Instantiate(CatInPrefab);
 
         CreentCatIn.transform.SetParent(canvas.transform, false);
-        iTween.MoveBy(CreentCatIn, iTween.Hash("y", -380, "time", 6.0f));
+        iTween.MoveBy(CreentCatIn, iTween.Hash("y", -500, "time", 6.0f));
         iTween.MoveAdd(CreentCatIn, iTween.Hash("y", 40, "time", 1.0f, "delay", 1.5f));
     }
 }
